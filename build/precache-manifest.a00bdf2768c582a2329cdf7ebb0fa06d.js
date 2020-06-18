79e48f88a101ed1ededceec4315aa7955f6e580e
Venkatesh <Harshith.Venkatesh@CenturyLink.com>
2020-06-19 00:13:19 +0530

    update

---
+++ build/precache-manifest.a00bdf2768c582a2329cdf7ebb0fa06d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83bdd78a44410d7360f0ea70e973f042",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abe994bc9cfcc4ea7d8a",
+    "url": "/static/css/main.db562463.chunk.css"
+  },
+  {
+    "revision": "2d6ffb2c9b8954488ed3",
+    "url": "/static/js/2.0c08033b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0c08033b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe994bc9cfcc4ea7d8a",
+    "url": "/static/js/main.2450f66b.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  },
+  {
+    "revision": "74509172bb2acd586e7e5bb7cd25a256",
+    "url": "/static/media/thunderstorm.74509172.jpg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { fetchWeather } from "./api/fetchData";
+import sunrise from "./Assets/sunrise.png";
+import sunset from "./Assets/sunset.png";
+// import PlacesAutocomplete, {
+//   geocodeByAddress,
+//   getLatLng,
+// } from "react-places-autocomplete";
+import "./App.css";
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
-function App() {
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      setWeather(data);
+      setQuery("");
+    }
+  };
+
+  //   const handlePlace = async (value) => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <React.Fragment>
+      <div className="main-container">
+        {/* <PlacesAutocomplete
+          value={query}
+          onChange={setQuery}
+          onSelect={handlePlace}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {({
+            getInputProps,
+            suggestions,
+            getSuggestionItemProps,
+            loading,
+          }) => (
+            <div>
+              <input
+                type="text"
+                className="search"
+                {...getInputProps({ placeholder: "Search...." })}
+                id="searchTerm"
+                // onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={search}
+              />
+              <div>
+                {loading ? <div>...loading</div> : null}
+                {suggestions.map((suggestion) => {
+                  return <div>{suggestion.description}</div>;
+                })}
+              </div>
+            </div>
+          )}
+        </PlacesAutocomplete> */}
+        <input
+          type="text"
+          className="search"
+          placeholder="Search...."
+          id="searchTerm"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyPress={search}
+        />
+
+        {weather.main && (
+          <div className="city">
+            <h2 className="city-name">
+              <span>{weather.name} </span>
+              <sup>{weather.sys.country} </sup>
+            </h2>
+            <div className="city-temp">
+              <span>{Math.round(weather.main.temp - 273.15)}</span>
+              <sup>&deg;C</sup>
+            </div>
+            <div className="info">
+              <img
+                className="city-icon"
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt={weather.weather[0].description}
+              />
+              <p>{weather.weather[0].description}</p>
+            </div>
+            <div>
+              <div style={{ display: "inline-block", float: "left" }}>
+                <img
+                  className="city-icon-time"
+                  src={sunrise}
+                  alt={weather.weather[0].description}
+                />
+
+                <p className="p-fontStyle">
+                  {new Date(weather.sys.sunrise * 1000).toLocaleTimeString(
+                    "en-US"
+                  )}
+                </p>
+              </div>
+              <div
+                style={{
+                  display: "inline-block",
+                  float: "left",
+                  marginLeft: "15px",
+                }}
+              >
+                <img
+                  className="city-icon-time"
+                  src={sunset}
+                  alt={weather.weather[0].description}
+                />
+
+                <p className="p-fontStyle">
+                  {new Date(weather.sys.sunset * 1000).toLocaleTimeString(
+                    "en-US"
+                  )}
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
